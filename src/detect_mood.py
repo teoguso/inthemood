03cb821334ba088931dea696d2f13fe219f2dd25
Matteo <guzzo.matteo@gmail.com>
2016-10-15 00:09:12 +0200

    The Heuro class needs username and password. Added.

--- src/detect_mood.py
+++ src/detect_mood.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 from heuro_api import Heuro
+from credentials import email, password
 
 def main():
-    myhero = Heuro()
+    myhero = Heuro(email, password)
     mypipe = myhero.make_pipeline(pipeline="pip1")
 
 if __name__ == "__main__":
