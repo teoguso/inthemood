fc587e954a01821f1b2d97a70212121bed9e5024
Peter Nilson <petenilson@gmail.com>
2016-10-16 21:30:13 +0200

    introducing watson
    added vis_rec_watson which uses the watson api,

--- src/test_webcam.py
+++ src/test_webcam.py
@@ -15,6 +15,8 @@ while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     frame_count +=1
+    if frame_count%100 == 0:
+        upload_image(cv2.
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
