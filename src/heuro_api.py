c11f443074beaed3c8c42947439125e56c900f94
Peter Nilson <petenilson@gmail.com>
2016-10-17 17:13:50 +0200

    updated heuro api
    added function to fetch open pipes
    improved error handling in ingest file

--- src/heuro_api.py
+++ src/heuro_api.py
@@ -40,8 +40,34 @@ class Heuro(object):
                     'User': self.id,
                     'Key' : self.key
                 }
-        r = self.session.post(url, data=json.dumps(data), headers=headers).json()
-        return r
+        # fetch a list of our existing pipelines to prevent us creating duplicates
+        open_pipes = [pipe['name'] for pipe in self.get_pipeline()]
+        if pipeline not in open_pipes:
+            r = self.session.post(url, data=json.dumps(data), headers=headers)
+            if r.status_code not in [200, 201]:
+                return r.json()
+            else:
+                raise Exception('Error creating pipeline, response: {}'.format(r.status_code))
+        else:
+            raise Exception('Error creating pipeline, {} already exists'.format(pipeline))
+
+    def get_pipeline(self, pipeline=None):
+        """
+        Fetches all pipelines available
+        : inputs: a pipeline name as a string
+        : side-effects: None
+        : returns: returns a list of dicts of pipelines if pipeline is not specified
+        """
+        url = 'http://api.cognitio.heurolabs.com/v1/pipelines'
+        headers = {'Content-Type': 'application/json',
+                    'User': self.id,
+                    'Key' : self.key
+                }
+        r = self.session.get(url, headers=headers)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise Exception('Error with get_pipelines, response: {}'.format(r.status_code))
 
     def ingest_file(self, filename, pipe_id):
         url = 'http://api.cognitio.heurolabs.com/v1/pipelines/{}/ingestfile'.format(pipe_id)
@@ -49,8 +75,11 @@ class Heuro(object):
                 }
         with open(filename, 'rb') as open_file:
             file = {'file': open_file}
-            r = self.session.post(url, headers=headers, files=file).json()
-            return r
+            r = self.session.post(url, headers=headers, files=file)
+            for r.status_code in [200, 201]:
+                return r.json()
+            else:
+                raise Exception('Error ingesting file, response: {}'.format(r.status_code))
 
     def get_results(self, pipeline_key, pipeline_id):
         url = 'http://api.cognitio.heurolabs.com/v1/pipelines/{}/results/query'.format(pipeline_key)
--- src/test_audio_ingest.py
+++ src/test_audio_ingest.py
@@ -1,7 +1,13 @@
 from heuro_api import Heuro
-from credentials import email, password, pipeline_id
+from credentials import email, password
 
 myhero = Heuro(email, password)
+audio_pipe = 1747
 
-pipe = myhero.make_pipeline('test_pipe123')
-print pipe
+#ingest = myhero.ingest_file('../data/audio/tom_scott_trim.mp3', audio_pipe)
+pipelinekey = 'K_ba0b77e0_de9c_41ee_9c30_1927ba646790'
+#pipelinekey = ingest['pipelinekey']
+#print pipelinekey
+
+print myhero.get_results(audio_pipe, pipelinekey).json()
+
