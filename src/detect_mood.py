f7edd834e1e322011e8de42d765786e9842ae4b1
Matteo <guzzo.matteo@gmail.com>
2016-10-18 13:46:51 +0200

    A bit more streamlining.
    Now we can handle audio and image together,
    and detect when the API doesn't find anything.

--- src/detect_mood.py
+++ src/detect_mood.py
@@ -4,25 +4,43 @@ from credentials import email, password, pipeline_id
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimage
 # from os import path
-# import json
 # import numpy as np
 import time
 import os
+import sys
 
 
-def analyse_file_api(file_to_ingest, target_api, pipeline_id):
-    upload = target_api.ingest_file(file_to_ingest, pipeline_id)
-    pipeline_key = upload['pipelinekey']
+def analyse_file_api(files_to_ingest, target_api, pipeline_id):
+    """
+    Tries to mimic a socket ingesting files and requesting results
+    in pseudo-parallel.
+    :param files_to_ingest:
+    :param target_api:
+    :param pipeline_id:
+    :return: a list of results for each ingestion
+    """
+    uploads = []
+    pipe_keys = []
+    for afile in files_to_ingest:
+        print("Ingesting file {}...".format(afile))
+        myup = target_api.ingest_file(afile, pipeline_id)
+        uploads.append(myup)
+        pipe_keys.append(myup['pipelinekey'])
     tot_t = 0
     dt = 3
-    results = target_api.get_results(pipeline_id, pipeline_key)
-    while results.status_code == 202:
+    stats = [target_api.get_results(pipeline_id, key).status_code for key in pipe_keys]
+    # while results.status_code == 202: target_api.get_results(pipeline_id, pipe_keys[0])
+    # print(stats)
+    while any([s == 202 for s in stats]):
         time.sleep(dt)
         tot_t += dt
-        results = target_api.get_results(pipeline_id, pipeline_key)
+        results = [target_api.get_results(pipeline_id, key) for key in pipe_keys]
+        stats = [target_api.get_results(pipeline_id, key).status_code for key in pipe_keys]
+        # for key in pipe_keys:
+        #     results.append(target_api.get_results(pipeline_id, key))
     print("Total time (secs):", tot_t)
-    r = results.json()
-    print("Status:", r['status'])
+    r = [res.json() for res in results]
+    # print("Status:", r['status'])
     return r
 
 def detect_jpeg(file_to_ingest):
@@ -35,6 +53,11 @@ def detect_jpeg(file_to_ingest):
 
 def main():
     myhero = Heuro(email, password)
+    # Small dictionary to convert API output to a readable format
+    mygender = {
+        0: "female",
+        1: "male"
+    }
     #mypipe = myhero.make_pipeline(pipeline="test_pipe")
     #print(mypipe)
     # upload = myhero.ingest_file('../data/images/test_faces_2.jpg', pipeline_id)
@@ -42,23 +65,40 @@ def main():
     file_to_ingest = '../data/audio/man-woman.mp3'
     file_to_ingest = '../data/images/four-in-car.jpeg'
     file_to_ingest = '../data/images/family1.jpeg'
+    audio_to_ingest = '../data/audio/man-woman.mp3'
+    audio_to_ingest = '../data/video/tagueule.mp3'
+    image_to_ingest = '../data/video/tagueule.png'
     """
+
+
     # Image first, just because
-    image_to_ingest = '../data/images/two-in-car.jpg'
+    image_to_ingest = "../data/images/thb1.jpg"
+    audio_to_ingest = "../data/audio/baby-talk.mp3"
+    files_to_ingest = [image_to_ingest, audio_to_ingest]
+
     if image_to_ingest[-3:] != "png" and \
                     image_to_ingest[-3:] != "jpg" and \
-                    image_to_ingest[-4:] != "jpeg":
-        sys.exit(" Sorry. Image file format is not recognized.")
+                    image_to_ingest[-4:] != "jpeg" and \
+            audio_to_ingest[-3:] != "mp3":
+        sys.exit(" Sorry. File format is not recognized.")
 
     is_jpeg = detect_jpeg(image_to_ingest)
-    r1 = analyse_file_api(image_to_ingest, target_api=myhero, pipeline_id=pipeline_id)
+    results = analyse_file_api(files_to_ingest, target_api=myhero, pipeline_id=pipeline_id)
+    # print(results)
+    # r1 = analyse_file_api(image_to_ingest, target_api=myhero, pipeline_id=pipeline_id)
+    r1 = results[0]
+    watch_json = True
+    if watch_json:
+        import json
+        with open('jsonwatch_image.txt', 'w') as outf:
+            json.dump(r1, outf, sort_keys = True, indent = 4, separators=(',', ':'))
 
     # Audio second
-    audio_to_ingest = '../data/audio/man-woman.mp3'
     if audio_to_ingest[-3:] != "mp3":
         sys.exit(" Sorry. Image file format is not recognized.")
 
-    r2 = analyse_file_api(audio_to_ingest, target_api=myhero, pipeline_id=pipeline_id)
+    # r2 = analyse_file_api(audio_to_ingest, target_api=myhero, pipeline_id=pipeline_id)
+    r2 = results[1]
 
     # Distinguish between image and audio (different output formats)
     inference_audio = {}
@@ -67,7 +107,12 @@ def main():
     r_video = r1['result']['output']['classification']
     # for x, y in zip(r_output.keys(), r_output.values()):
     #     print(str(x)+": "+str(np.array(y).shape)+str(y))
-    people = r_video['objects']
+    people = r_video.get('objects')
+
+    # Test if we got anything at all out
+    if not people:
+        sys.exit("Error: the API did not detect any people.")
+
     n_people = len(people)
     gender_image = []
     age_image = []
@@ -90,8 +135,6 @@ def main():
     # elif file_to_ingest[-3:] == "png" or\
     #                 file_to_ingest[-3:] == "jpg" or\
     #                 file_to_ingest[-4:] == "jpeg":
-        # with open('jsonwatch_image.txt', 'w') as outf:
-        #     json.dump(r, outf, sort_keys = True, indent = 4, separators=(',', ':'))
 
     print("=== Audio evaluation ===")
     print("gender_audio: ", gender_audio)
@@ -114,8 +157,8 @@ def main():
     plt.imshow(img)
 
     for age, gend, face in zip(age_image, gender_image, face_xy):
-        plt.text(face[0],face[1], str(age)+" "+str(gend), color='black', \
-                 backgroundcolor='black', fontsize=24)
+        plt.text(face[0],face[1], str(age)+" "+str(mygender[gend]), color='black', \
+                 fontsize=24) #, alpha=.8)
     plt.show()
     # Remove png file, only needed for visualization
     if is_jpeg:
